c58ef2d33b6742e0ab8b6ca7bb68e6f15ea89eb8
Daniel Rosa <37475341+LordAkirast@users.noreply.github.com>
2026-01-16 15:33:56 -0300

    Update and rename teste.txt to teste.js

---
+++ teste.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Teste GIT
+// @namespace    http://tampermonkey.net/
+// @version      2026-01-16
+// @description  A script that updates automatically from GitHub
+// @author       You
+// @match        https://example.com/*
+// @updateURL    https://github.com/LordAkirast/tamperScripts
+// @downloadURL  https://github.com/LordAkirast/tamperScripts
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+alert('oi')
+    // Your code here...
+})();
